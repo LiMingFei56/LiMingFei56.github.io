b09c45157814231a9e2d5cbb1048cc813625365e
LiMingFei <757563349@qq.com>
2018-11-29 14:44:21 +0800

    del mydiscuss

--- mydiscuss/CartoonPlay/app/src/main/java/com/lmf/cartoonplay/model/Dir.kt
+++
@@ -1,3 +0,0 @@
-package com.lmf.cartoonplay.model
-
-data class Dir(val name: String, val parentDir: String, val iconUrl: String, val isVedio: Boolean)
--- mydiscuss/CartoonServer/CartoonFileManager/urls.py
+++
@@ -1,7 +0,0 @@
-from django.conf.urls import url
-from . import views
-
-urlpatterns = [
-    url(r'path', views.path, name='path'),
-    url(r'video', views.video, name='video'),
-]
